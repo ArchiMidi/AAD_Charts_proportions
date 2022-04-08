03e4155700e10bf8232844f9951de9929ee5056e
prod.astri <58519454+prod-astri@users.noreply.github.com>
2022-04-08 11:33:58 +0200

    cleanup and import data

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import HomePage from './pages/HomePage';
 import ErrorPage from './pages/ErrorPage';
-import SecondPage from './pages/SecondPage'
 
 function App() {
   function sumMixedValuesArray(arr) {
@@ -17,7 +16,7 @@ function App() {
   const location = useLocation();
   const routes = [
     { path: '/', element: <HomePage  sumMixedValuesArray={sumMixedValuesArray} />, name: 'Home' },
-    { path: '/second', element: <SecondPage sumMixedValuesArray={sumMixedValuesArray}/>, name: 'Home' },
+    // { path: '/second', element: <SecondPage sumMixedValuesArray={sumMixedValuesArray}/>, name: 'Home' },
    
     { path: '/*', element: <ErrorPage />, name: '404' },
   ]
@@ -41,17 +40,3 @@ function App() {
 }
 
 export default App;
-
-
-
-// NOTES:
-// - not working from v6:
-//   import { Switch, Route } from 'react-router-dom'
-//   <Switch>
-//     <Route path="/" component={HomePage} />
-//     <Route component={ErrorPage} />  
-//   </Switch>
-//   info at https://stackoverflow.com/questions/63124161/attempted-import-error-switch-is-not-exported-from-react-router-dom/69849271#69849271
-
-//   transitionGroup behaves differently:
-//   working example at: https://codesandbox.io/s/6l1li?file=/src/index.tsx:790-821
---
+++ src/utils/mapArr.js
@@ -0,0 +1,6 @@
+import data from '../assets/AAD_organised'
+
+export default function mapArr() {
+    console.log(data)
+return ['a', 'b']
+}
