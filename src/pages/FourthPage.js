3bbf627df2e613e8592b5d240c2ab692976c8009
prod.astri <58519454+prod-astri@users.noreply.github.com>
2022-04-12 15:55:01 +0200

    mk fourth page

---
+++ src/pages/FourthPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FourthPage(props) {
+    const { dataArr }  = props
+    
+  return (
+    <div className='forthContainer'>FourthPage</div>
+  )
+}
