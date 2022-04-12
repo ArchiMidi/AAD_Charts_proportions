a929c6d8fd295f530a3bbf9d0924a48db185f963
prod.astri <58519454+prod-astri@users.noreply.github.com>
2022-04-12 16:20:52 +0200

    create sqrt(2) containers for A4

--- src/pages/FourthPage.js
+++ src/pages/FourthPage.js
@@ -1,9 +1,29 @@
 import React from 'react'
 
 export default function FourthPage(props) {
-    const { dataArr }  = props
-    
-  return (
-    <div className='forthContainer'>FourthPage</div>
-  )
+    const { dataArr } = props
+
+    const maxAmountOfExhibitions = dataArr.map(el => el[1].totalNumberOfExhibitions).reduce((acc, val)=>{
+        return val > acc ? val : acc
+    })
+
+    const gridColumns = () => {
+        return 'min-content ' + dataArr[0][1].yearsValues.map((el, i) => '5rem ').join('')
+    }
+    const gridRows = () => {
+        return dataArr.map((el, i) => '5rem ').join('')
+    }
+
+
+    return (
+        <div className='fourthContainer'>
+        <h3>AAD_CHART</h3>
+        <div className= 'chartContainerPageFour'>
+
+            {dataArr.map((el) => {
+                return <>el</>
+            })}
+        </div>
+        </div>
+    )
 }
