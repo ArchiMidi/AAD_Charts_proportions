5169e5c5e25b4c6a8f6f4ce2787a195b877dd0d3
prod.astri <58519454+prod-astri@users.noreply.github.com>
2022-04-14 17:20:47 +0200

    add triangles; fix positions within grid;

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import A4Container from '../components/A4Container'
 import PlaceNameBox from '../components/PlaceNameBox'
+import { ReactComponent as Triangle } from '../assets/triangle.svg'
+
+
 export default function HomePage(props) {
     const { dataArr } = props
 
@@ -9,6 +12,10 @@ export default function HomePage(props) {
     }
     const gridRows = () => {
         return "1fr" + dataArr.map((el, i) => '1fr ').join('')
+    }
+
+    function calculateTriangleSide(area) {
+        return Math.sqrt(4 * area / 1.7320508075688772) * 10
     }
 
     return (
@@ -38,14 +45,37 @@ export default function HomePage(props) {
                 return (
                     <div
                         className='chartBox' style={{
+                            // position:'absolute',
                             gridColumn: 2,
                             gridRow: i + 2,
                             width: '100%',
+                            
                         }}
                         key={i + 'chart'}
                     >
-                    <div className='chartLine'/>
-                        {info.yearsValues}
+                        <div className='chartLine' />
+                        
+
+                            {info.yearsValues.map((val, i) => {
+                                return (
+                                    <Triangle
+                                        key={name + val + i}
+                                        width={(val !== 0 ? calculateTriangleSide(10) : 10)}
+                                        style={{
+                                            position: 'absolute',
+                                            left: `${i * 3}rem`,
+                                            borderLeft: '1px solid pink',
+                                            bottom: (val !== 0 ? "0rem" : -10),
+                                            stroke: 'white',
+                                            strokeWidth: '0.08rem',
+                                            strokeLinejoin: 'round',
+                                            fill: val === 0 && "red",
+                                            transform: val === 0 && 'scaleY(-1)'
+
+                                        }} />
+                                )
+                            })}
+                        
                     </div>
                 )
             })}
