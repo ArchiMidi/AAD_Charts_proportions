d51f8ae91600e85d3c2cfc158f6acaefa72a94a1
prod.astri <58519454+prod-astri@users.noreply.github.com>
2022-04-17 11:35:16 +0200

    add rects style

--- src/pages/Rectangles.js
+++ src/pages/Rectangles.js
@@ -6,22 +6,28 @@ export default function Rectangles(props) {
     const { dataArr } = props
 
     return (
-        <div>
+        <div style={{
+            display: 'flex',
+            flexDirection: 'row-reverse',
+            flexWrap: 'wrap-reverse',
+            width: '200vw',
+            height:'',
+        }}>
 
             {dataArr.map((place, i) => {
                 const name = place[0];
                 const info = place[1];
-                const height = info.totalNumberOfExhibitions * 100
+                const height = info.totalNumberOfExhibitions / 5
                 return (
                     <div className='proportionateFrame' key={'frame' + name}
                         style={{
-                            width: '100px',
-                            height: `${height}px`,
-                            margin: '10px',
-                            backgroundColor: 'pink',
+                            minWidth: '30vw',
+                            height: `${height}vw`,
+                            margin: '1vw',
+                            backgroundColor: 'pink'
                         }}
                     >
-                        {/* {name} */}
+                        {name}
                     </div>
                 )
             })}
